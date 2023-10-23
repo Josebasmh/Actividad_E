ef08b35dabc3e791cdd514eefefb3a409ecb106a
Joseba <117350714+Josebasmh@users.noreply.github.com>
2023-10-23 19:46:18 +0200

    Squashed commit of the following:
    commit c0f78a8437f5218c59b525f23c4a6a4d2d307661
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Mon Oct 23 19:45:39 2023 +0200
    
        FIX-Compatibilidad
    
        Arreglar los problemas con las librerías y los JAR externos.
    
    commit 94e6d978def4702057eb8f2234688480e91d9a5d
    Author: Joseba <117350714+Josebasmh@users.noreply.github.com>
    Date:   Mon Oct 23 19:39:55 2023 +0200
    
        ADD-Actividad_D
    
        Importación de datos de Actividad_D

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,16 @@
  *
  */
 module Actividad_E {
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires javafx.base;
+	
+	opens model to javafx.graphics, javafx.fxml, javafx.base;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+	opens controller to javafx.graphics, javafx.fxml, javafx.base;
 }
